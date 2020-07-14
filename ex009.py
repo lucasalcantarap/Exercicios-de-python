197eab9bb8cf73cf73df16673374d8d991492782
Lucas Alcantara <lucaspe0685@gmail.com>
2020-07-13 22:23:26 -0300

    adição de exercícios

---
+++ ex009.py
@@ -0,0 +1,4 @@
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+media = (nota1+nota2)/2
+print('A média9 das notas é {}'.format(media)) 
---
+++ ex010.py
@@ -0,0 +1,22 @@
+n = int(input('Digite um número para saber sua tabuada: '))
+res1 = n*1
+res2 = n*2
+res3 = n*3
+res4 = n*4
+res5 = n*5
+res6 = n*6
+res7 = n*7
+res8 = n*8
+res9 = n*9
+res10 = n*10
+
+print(n,'x 1 = {}'.format(res1))
+print(n,'x 2 = {}'.format(res2))
+print(n,'x 3 = {}'.format(res3))
+print(n,'x 4 = {}'.format(res4))
+print(n,'x 5 = {}'.format(res5))
+print(n,'x 6 = {}'.format(res6))
+print(n,'x 7 = {}'.format(res7))
+print(n,'x 8 = {}'.format(res8))
+print(n,'x 9 = {}'.format(res9))
+print(n,'x 10 = {}'.format(res10))
