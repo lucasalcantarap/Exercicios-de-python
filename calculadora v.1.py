52769df43c75759999346d9946c81b9b56b205a1
Lucas Alcantara <lucaspe0685@gmail.com>
2020-07-24 20:30:18 -0300

    calculadora v.1

---
+++ calculadora v.1.py
@@ -0,0 +1,24 @@
+print("--------CALCULADORA V 1.0--------")
+sair = False
+
+while sair == False:
+
+  n1 = int(input('Digite o primeiro número: '))
+  operador = input("Digite um operador ( + - * /) ")
+  n2 = int(input('Digite um segundo número: '))
+
+  if operador == '+' :
+    print(n1+n2)
+
+  if operador == '-' :
+    print(n1-n2)
+
+  if operador == '*' :
+    print(n1*n2)
+
+  if operador == '/' :
+    print(n1/n2)
+
+  teste = input('Deseja sair(s/n)? ')
+  if teste == 's':
+    sair = True
