8feb952599b40207f7f31b5451b518ff94269acd
Lucas Alcantara <lucaspe0685@gmail.com>
2020-08-02 22:19:05 -0300

    ad ex 013

--- ex013-desconto-de-produto.py
+++
@@ -1,3 +0,0 @@
-preco = float(input('Digite o valor do produto: R$ '))
-desconto = preco * 0.05
-print('O preço do produto é R${} e com 5% de desconto fica R$ {:.2f} e com desconto fica R$ {:.2f}'.format(preco,desconto,preco - desconto))
---
+++ ex014 - calculo-de-salario15%.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual é o valor do salário ? R$ '))
+salario_aumento = salario + (salario * 0.15)
+print('Salário anterior R$ {} e agora com 15% de aumento passa a ser R$ {}'.format(salario, salario_aumento))
