025d13791a61fa3125698dc15fd0d7107cbad1ca
lucasalcantarap <lucaspe0685@gmail.com>
2021-01-27 22:46:26 -0300

    add funcao lambda

--- aula8_lambda.py
+++ aula8_lambda.py
@@ -3,4 +3,10 @@ contador_letras = lambda lista: [len(x) for x in lista]
 lista_animais = ['cachorro', 'gato','rato']
 print(contador_letras(lista_animais))
 
-# 22:20 min de video
+
+soma = lambda a, b: a + b
+subtracao = lambda a, b: a- b
+print(soma(5,10))
+print(subtracao(10,5))
+
+# 23:05 min de video
